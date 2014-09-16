84cc1a00e0e3744b07c4272c7e331adf28d1c679
Tomasz Morcinek <tomasz.morcinek@gmail.com>
2014-09-16 15:09:58 +0200

    Retriving package name from AndroidManifest.xml file.

--- com.morcinek.android.codegenerator.plugin/src/main/java/com/morcinek/android/codegenerator/plugin/Convert.java
+++ com.morcinek.android.codegenerator.plugin/src/main/java/com/morcinek/android/codegenerator/plugin/Convert.java
@@ -1,6 +1,8 @@
 package com.morcinek.android.codegenerator.plugin;
 
 import com.morcinek.android.codegenerator.CodeGenerator;
+import com.morcinek.android.codegenerator.extractor.PackageExtractor;
+import com.morcinek.android.codegenerator.extractor.XMLPackageExtractor;
 import com.morcinek.android.codegenerator.extractor.XMLResourceExtractor;
 import com.morcinek.android.codegenerator.extractor.string.FileNameExtractor;
 import com.morcinek.android.codegenerator.plugin.editor.CodeDialog;
@@ -11,14 +13,19 @@ import com.morcinek.android.codegenerator.writer.templates.ResourceTemplatesProv
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.internal.resources.File;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 
+import java.io.FileInputStream;
 import java.io.InputStream;
 
 
@@ -39,7 +46,8 @@ public class Convert extends AbstractHandler {
             String string = codeGenerator.produceCode(fileContents, resource.getName());
             final IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(arg0);
 
-            CodeDialog dialog = new CodeDialog(window.getShell(), resource.getName(), "com.morcinek.package", string);// new input dialog
+            String packageName = getPackageName(getRootPath(selection));
+            CodeDialog dialog = new CodeDialog(window.getShell(), resource.getName(), packageName, string);// new input dialog
             if (dialog.open() == IStatus.OK) {
                 //TODO
             } else {
@@ -62,5 +70,23 @@ public class Convert extends AbstractHandler {
 
     private void showErrorMessage(String message) {
         MessageDialog.openError(new Shell(), "Eclipse Maven Plugin", message);
+    }
+
+    private String getRootPath(IStructuredSelection selection) {
+        File fileProject = (File) selection.getFirstElement();
+        IWorkspace workspace = ResourcesPlugin.getWorkspace();
+        java.io.File workspaceDirectory = workspace.getRoot().getLocation().toFile();
+        Path p = new Path(workspaceDirectory.getAbsolutePath() + fileProject.getProject().getFullPath());
+        return p.toOSString();
+    }
+
+    private String getPackageName(String rootPath) {
+        Path path = new Path(rootPath + "/AndroidManifest.xml");
+        try {
+            return new XMLPackageExtractor().extractPackageFromManifestStream(new FileInputStream(path.toFile()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
