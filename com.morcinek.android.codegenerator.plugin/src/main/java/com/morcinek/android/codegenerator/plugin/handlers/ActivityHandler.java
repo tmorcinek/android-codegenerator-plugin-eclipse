1b8a812e993aaa5a2550e5741f8502344c9dc83d
Tomasz Morcinek <tomasz.morcinek@gmail.com>
2014-09-18 01:44:54 +0200

    Removing unused imports.

--- com.morcinek.android.codegenerator.plugin/src/main/java/com/morcinek/android/codegenerator/plugin/handlers/ActivityHandler.java
+++ com.morcinek.android.codegenerator.plugin/src/main/java/com/morcinek/android/codegenerator/plugin/handlers/ActivityHandler.java
@@ -1,11 +1,5 @@
 package com.morcinek.android.codegenerator.plugin.handlers;
 
-import com.morcinek.android.codegenerator.codegeneration.providers.factories.ActivityResourceProvidersFactory;
-import org.eclipse.core.commands.AbstractHandler;
-import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
-
-
 /**
  * Copyright 2014 Tomasz Morcinek. All rights reserved.
  */
--- com.morcinek.android.codegenerator.plugin/src/main/java/com/morcinek/android/codegenerator/plugin/handlers/AdapterHandler.java
+++ com.morcinek.android.codegenerator.plugin/src/main/java/com/morcinek/android/codegenerator/plugin/handlers/AdapterHandler.java
@@ -1,22 +1,5 @@
 package com.morcinek.android.codegenerator.plugin.handlers;
 
-import com.morcinek.android.codegenerator.CodeGenerator;
-import com.morcinek.android.codegenerator.codegeneration.providers.factories.AdapterResourceProvidersFactory;
-import com.morcinek.android.codegenerator.plugin.Activator;
-import com.morcinek.android.codegenerator.plugin.eclipse.CodeDialog;
-import com.morcinek.android.codegenerator.plugin.eclipse.EnvironmentHelper;
-import com.morcinek.android.codegenerator.plugin.utils.ClipboardHelper;
-import com.morcinek.android.codegenerator.plugin.utils.PackageNameHelper;
-import com.morcinek.android.codegenerator.plugin.utils.PathHelper;
-import com.morcinek.android.codegenerator.plugin.utils.PreferencesHelper;
-import org.eclipse.core.commands.AbstractHandler;
-import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.ide.IDE;
-
 /**
  * Copyright 2014 Tomasz Morcinek. All rights reserved.
  */
